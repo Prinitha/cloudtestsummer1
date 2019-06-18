81776f69cd4f2d02c597e89bd4c295d05d7c4064
prinitha <nprinitha5@gmail.com>
2019-06-18 00:17:18 -0500

    Added Database

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import pypyodbc
 app = Flask(__name__)
-
+connection = pypyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:pxn8557.database.windows.net,1433;Database=DATABASE;Uid=prinitha@pxn8557.database.windows.net,1433;Pwd=chintu@1;")
 
 @app.route('/')
 def hello_world():
-    return 'Hello Prinitha Dabba!'
+    cursor = connection.cursor()
+    cursor.execute("select count(*) from all_month")
+    rows = cursor.fetchall()
+    count = rows[0][0]
+    return render_template('index.html', count=count)
 
 
 if __name__ == '__main__':
