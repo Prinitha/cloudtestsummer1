a51b20e921e6def69b010d6b02322a53c5a31633
PRINITHA NAGARAJAN <nprinitha5@gmail.com>
2019-06-18 13:22:54 -0500

    Redis

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
 import random
 import pypyodbc
+import redis
 from time import time
 app = Flask(__name__)
 connection = pypyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:pxn8557.database.windows.net,1433;Database=DATABASE;Uid=prinitha@pxn8557.database.windows.net,1433;Pwd=chintu@1;")
 cursor = connection.cursor()
 app.secret_key = "Secret!!!!"
+host_name = 'cloudredis.redis.cache.windows.net:6380'
+password = 'Nbez9mJY7hUbFWLFAgWp7OJXyku9XIXep7waffZg4z8='
+cache = redis.StrictRedis(host=host_name, port=6380, password=password, ssl=True)
+
 
 @app.route('/')
 def hello_world():
@@ -35,8 +40,12 @@ def query_specific():
     start_time = time()
     for i in range(0, int(query_limit)):
         magnitude = random.uniform(float(lower_limit), float(higher_limit))
-        sql = 'select * from all_month where mag>=? '
-        cursor.execute(sql, (magnitude,))
+        if not cache.get(magnitude):
+            sql = 'select * from all_month where mag>=? '
+            cursor.execute(sql, (magnitude,))
+            cache.set(magnitude, cursor.fetchall())
+        else:
+            rows = cache.get(magnitude)
     end_time = time()
     time_taken = (end_time - start_time) / int(query_limit)
     flash('The Average Time taken to execute the specific queries is : ' + "%.4f" % time_taken + " seconds")
