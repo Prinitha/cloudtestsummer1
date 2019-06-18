ee99a12f6894ddd48bc282aea31da7f83e7b6c52
PRINITHA NAGARAJAN <nprinitha5@gmail.com>
2019-06-18 17:17:26 -0500

    space

--- app.py
+++ app.py
@@ -65,11 +65,11 @@ def query_specific():
             cursor.execute(sql, (magnitude,))
             rows = cursor.fetchall()
             cache.set(magnitude, str(rows))
-            flash('In DB Query'+str(magnitude))
+            flash('In DB Query '+str(magnitude))
         else:
             rows_string = cache.get(magnitude)
             # rows = ast.literal_eval(rows_string)
-            flash('In Cache' + str(magnitude))
+            flash('In Cache ' + str(magnitude))
     end_time = time()
     time_taken = (end_time - start_time) / int(query_limit)
     flash('The Average Time taken to execute the specific queries is : ' + "%.4f" % time_taken + " seconds")
