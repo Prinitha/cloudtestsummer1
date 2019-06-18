d7fdba57e87584e101357433f922af9b7d8b883d
PRINITHA NAGARAJAN <nprinitha5@gmail.com>
2019-06-18 12:30:47 -0500

    fixed

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import pypyodbc
 from time import time
 app = Flask(__name__)
 connection = pypyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:pxn8557.database.windows.net,1433;Database=DATABASE;Uid=prinitha@pxn8557.database.windows.net,1433;Pwd=chintu@1;")
-
+cursor = connection.cursor()
 
 @app.route('/')
 def hello_world():
-    cursor = connection.cursor()
     cursor.execute("select count(*) from all_month")
     rows = cursor.fetchall()
     count = rows[0][0]
@@ -18,7 +17,7 @@ def hello_world():
 def random_queries():
     query_limit = request.args['nqueries']
     start_time = time()
-    cursor = connection.cursor()
+    print(query_limit)
     for i in range(0, int(query_limit)):
         cursor.execute('select TOP 1 * from all_month order by rand()')
     end_time = time()
