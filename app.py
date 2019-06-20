d97395fbb5f2af82edffcf1d2fb7c567e9ffb8bc
PRINITHA NAGARAJAN <nprinitha5@gmail.com>
2019-06-20 01:01:15 -0500

    cgb

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def question1():
 
 def question1_execute():
     bar_chart = pygal.Bar(width=1000, height=500)
-    sql = "select latitude,depth from quake6"
+    sql = "select latitude,depth from quake6" 
     # print(sql)
     cursor = conn.cursor()
     result = cursor.execute(sql).fetchall()
